73cd883771aa2eb1868d2f4ccfa2aa1876961e5b
794473 <794473@ELF8IT221607.ACDM.DS.SAIT.CA>
2019-09-06 15:57:42 -0600

    New Project Creation Created a new Java Application for HelloWorld.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,7 +14,9 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-         }
+    public static void main(String[] args)
+    {
+         System.out.println("Hello World");
+    }
     
 }
